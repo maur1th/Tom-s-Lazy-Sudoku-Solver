917ef273e53eb177956a4b4dbef80fcab3e95d18
Thomas Maurin <maurin.th@gmail.com>
2014-09-02 03:17:11 +0200

    CS50 submission

--- board_util.py
+++ board_util.py
@@ -1,19 +1,74 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Utility functions for basic board management
+# Thomas Maurin, 2014
+# Tom's Lazy Sudoku Solver
+# -- Helper file
 
+import sys
 import os
+import string
+
+
+def error(kind):
+    """ Handle various kinds of errors
+    """
+    if kind == 2:
+        print "Please enter a valid file (refer to examples for format)."
+    elif kind == 3:
+        print "Please provide an existing file."
+    elif kind == 8:
+        print "Unsolvable, please check your input and/or try again, you might be luckier!"
+    else:
+        print "Usage: lazy_sudoku.py puzzle.txt (refer to examples for format)"
+        print "       or lazy_sudoku.py --manual_input"
+    sys.exit(1)
+
+
+def validate(file):
+    """ Check the correctness of the file input
+    """
+    try:
+        f = open(file)
+    except:
+        error(3)
+
+    # Parse f line by line and char by char and check if 1-9 or space
+    y = 0
+    for line in f:
+        x = 0
+        for char in line:
+            try:
+                char = int(char)
+            except:
+                pass
+            if char == None:
+                break
+            elif char in range(1,10) or char.isspace():
+                pass
+            else:
+                error(2)
+            x += 1
+        y += 1
+        if x != 10:
+            if x == 9 and y == 9:
+                continue
+            else:
+                error(2)
+
+    if y != 9:
+        error(2)
+
+    f.close()
+    return None
 
 
 def setboard(file):
-    """Set the 9x9 grid in a dict where keys are the coordinates for each cell
+    """ Set the 9x9 grid in a dict where keys are the coordinates for each cell
     """
     f = open(file)
     y = 1
     board = dict()
 
-    # Parse f line by line and char by char and store its content in
+    # Parse f line by line and char by char and store its contents in
     # a dict as follows '(x,y) : value' (x,y) being the coordinates of the cell
     for line in f:
         x = 1
@@ -22,7 +77,7 @@ def setboard(file):
                 break
             try:
                 board[(x, y)] = int(char)
-            except ValueError:
+            except:
                 board[(x, y)] = 0
             x += 1
         y += 1
@@ -30,42 +85,113 @@ def setboard(file):
     return board
 
 
-def manual_input():
-    """ Manual input of the sudoku board
+def setblank():
+    """ Set a blank board
     """
 
-    # Create an empty board (9x9 filled with zeroes) with x and y the
-    # coordinates of each cell/number
+    # Set a blank board
     board = dict()
-    for y in xrange(1, 10):
-        for x in xrange(1, 10):
-            board[(x, y)] = 0
-    
-    # Ask for input for each cell
-    for y in xrange(1, 10):
-        for x in xrange(1, 10):
+    for y in xrange(1,10):
+        for x in xrange(1,10):
+            board[(x,y)] = 0
+
+    for y in xrange(1,10):
+        for x in xrange(1,10):
             os.system('cls' if os.name == 'nt' else 'clear')
-            board[(x, y)] = "_"
+            board[(x,y)] = '_'
+
+            print "Please enter your board:"
             printboard(board)
-            while True:
-                try:
-                    num = raw_input("Value (1-9)? Enter to skip: ")
-                    if not num:
-                        board[(x, y)] = 0
-                        break
-                    else:
-                        num = int(num)
-                        if num in xrange(1, 10):
-                            board[(x, y)] = num
-                            break
-                        continue
-                except ValueError:
-                    continue
+
+            board[(x,y)] = input()
+
     return board
 
 
+def input():
+    """ Manage input on a manually entered board
+    """
+    while True:
+        value = raw_input("Value? (1-9, enter to skip, q to quit) > ")
+
+        try:
+            value = int(value)
+        except:
+            pass
+
+        if value in range(1, 10):
+            return value
+        elif value == "":
+            return 0
+        elif value == "q":
+            sys.exit(0)
+        else:
+            continue
+
+
+def edit(board):
+    """ Edits the board
+    """
+
+    # Print the board with coordinates
+    print "Entered board:\n"
+
+    # Header
+    print ' ' * 3,
+
+    columns = list(string.ascii_uppercase)[:9]
+    count_cols = 0
+    for letter in columns:
+        count_cols += 1
+        if count_cols % 3 != 0:
+            print letter,
+        else:
+            print letter + '  ',
+    print
+    
+    print '  +' + '-' * 23 + '+'
+    # Body
+    for y in xrange(1,10):
+        print y, '|',
+
+        for x in xrange(1,10):
+            num = board[(x,y)]
+            if num == 0:
+                num = ' '
+            if x == 9:
+                print num, '|'
+            elif x % 3 == 0:
+                print num, '|',
+            else:
+                print num,
+        if y % 3 == 0 and y != 9:
+            print '  |' + '-' * 7 + '+' + '-' * 7 + '+' + '-' * 7 + '|'
+    # Footer
+    print '  +' + '-' * 23 + '+'
+    print
+
+    # Ask for modification
+    while True:
+        position = raw_input("Position to edit? (format: A3) > ")
+
+        try:
+            row = int(position[1:])
+        except:
+            continue
+
+        if position[0] in columns and row in range(1, 10):
+            x = columns.index(position[0])+1
+            y = row
+            
+            board[(x,y)] = input()
+            return board
+
+        else:
+            continue
+
+
 def printboard(board):
-    """ Print the sudoku board
+    """ Print the board
     """
 
     # Header
--- lazy_sudoku.py
+++ lazy_sudoku.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright 2014 Thomas Maurin
+# Thomas Maurin, 2014
 # Tom's Lazy Sudoku Solver
 
 import sys
 import os
 import board_util
-import engine
 from random import randint
 
 
@@ -17,7 +14,6 @@ class BreakLoop(Exception):
     """
     pass
 
-
 def restore_board(board, backup):
     """ Restore the board to its previous state
     """
@@ -26,34 +22,41 @@ def restore_board(board, backup):
     return board, backup
 
 
-# if len(sys.argv) < 2:
-#     print "Usage: sudoku.py puzzle.txt"
-#     sys.exit(1)
+if len(sys.argv) < 2 :
+    board_util.error(1)
 
+# Set a list from 1 to 9 to check against during evaluations
+values = [x for x in xrange(1, 10)]
 
-board = dict()
-if len(sys.argv) == 2:
-    board = board_util.setboard(sys.argv[1])
-else:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print "Welcome to Tom's Lazy Sudoku Solver!\n"
-    print "Would you like to:"
-    print "A. Input your own values?"
-    print "B. Link to a recorded grid?\n"
-
+# Initialize the board and do checks
+if sys.argv[1] == "--manual_input":
+    board = board_util.setblank()
     while True:
-        choice = raw_input("A/B ? > ")
-        if choice == "A":
-            board = board_util.manual_input()
+        # Print the board
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print "Entered board:"
+        board_util.printboard(board)
+        
+        # Enquire wether the board is correct
+        answer = raw_input("Is you board correct ? (y/n) > ")
+        if answer == "y":
             break
-        elif choice == "B":
-            path = raw_input("Path to the recorded grid: ")
-            board = board_util.setboard(path)
-            break
+        elif answer == "n":
+            os.system('cls' if os.name == 'nt' else 'clear')
+            board = board_util.edit(board)
         else:
             continue
 
-# Initialize the sudoku board and print it
+elif sys.argv[1][-4:] == ".txt":
+    board_util.validate(sys.argv[1])
+    board = board_util.setboard(sys.argv[1])
+
+else:
+    board_util.error(1)
+
+# Let's go!
+os.system('cls' if os.name == 'nt' else 'clear')
+print "Entered board:"
 board_util.printboard(board)
 
 # Store the coordinates of the initial values
@@ -62,14 +65,194 @@ for coordinates in board:
     if board[coordinates] != 0:
         initial_values.append(coordinates)
 
-board = engine.solver(board)
+# Set squares start coordinates (top-left)
+sqr_start = ['']
+for y in xrange(1, 10):
+    for x in xrange(1, 10):
+        if x % 3 == 1 and y % 3 == 1:
+            sqr_start.append([x, y])
 
-print 'Complete board:'
-board_util.printboard(board)
+# Define squares for each set of coordinates on the board
+square = {}
+for x in xrange(0, 9):
+    for y in xrange(0, 9):
+        square[(x + 1, y + 1)] = (x / 3 + 1) + (y / 3) * 3
 
+# Solver
+not_solved = True
+
+# List to keep track of previous states of the board in case a random
+# guess is wrong
+backup = list()
+
+rollbacks = 0
+
+while not_solved:
+
+    # Pointer to trigger the end of the main while loop when a solution is
+    # found
+    not_solved = False
+
+    # Pointer to trigger the random guess of a number when no result by
+    # Inclusion solver is possible
+    no_change = True
+
+    # Evaluate for each row which numbers are still to be added to the grid
+    not_in_rows = ['']
+    # Cycle over each row
+    for y in xrange(1, 10):
+        # Create a copy of the "values" list which is 1-9
+        not_in_row = list(values)
+        # Cycle over each column of the y row
+        for x in xrange(1, 10):
+            if board[(x, y)] in values:
+                # Each time a value is found, remove it from the list
+                try:
+                    not_in_row.remove(board[(x, y)])
+                except:
+                    board_util.error(8)
+        not_in_rows.append(not_in_row)
+
+    # Evaluate for each column which numbers are still to be added to the grid
+    not_in_cols = ['']
+    # Cycle over each column
+    for x in xrange(1, 10):
+        # Create a copy of the "values" list which is 1-9
+        not_in_col = list(values)
+        # Cycle over each row
+        for y in xrange(1, 10):
+            if board[(x, y)] in values:
+                # Each time a value is found, remove it from the list
+                try:
+                    not_in_col.remove(board[(x, y)])
+                except:
+                    board_util.error(8)
+        not_in_cols.append(not_in_col)
+
+    # Evaluate for each 3x3 square which numbers are still
+    # to be added to the grid
+    not_in_sqrs = ['']
+    # Cycle over each square determined by its "start" coordinates
+    # (which is the top left corner of each of the 9 squares of the grid)
+    for n in xrange(1, 10):
+        # Create a copy of the "values" list which is 1-9
+        not_in_sqr = list(values)
+        for y in xrange(0, 3):
+            for x in xrange(0, 3):
+                if (board[(sqr_start[n][0] + x, sqr_start[n][1] + y)]
+                        in values):
+                    # Each time a value is found, remove it from the list
+                    try:
+                        not_in_sqr.remove(board[(sqr_start[n][0] + x,
+                                                 sqr_start[n][1] + y)])
+                    except:
+                        board_util.error(8)
+        not_in_sqrs.append(not_in_sqr)
+
+    # Inclusion solver : for each cell of the grid, intersect the three lists
+    # of numbers still to be added (row, col, sqr)
+    try:
+        for y in xrange(1, 10):
+            for x in xrange(1, 10):
+                if isinstance(board[(x, y)], list) or board[(x, y)] == 0:
+
+                    not_solved = True
+
+                    # Intersection
+                    board[(x, y)] = list(set(not_in_rows[y])
+                                        & set(not_in_cols[x]) 
+                                        & set(not_in_sqrs[square[(x, y)]]))
+
+                    # Handle lists of length zero which result of a "bad
+                    # guess" below
+                    if len(board[(x, y)]) == 0:
+
+                        if len(backup) == 0:
+                            board_util.error(8)
+
+                        # Restore previously used board and delete last backup
+                        board, backup = restore_board(board, backup)
+
+                        # Increase a rollback counter for each error
+                        # encountered, every 10 (yes, it is somewhat 
+                        # arbitrary), restore the board to a
+                        # previous random state
+                        rollbacks += 1
+                        if rollbacks > 10:
+                            rollbacks = 0
+                            for x in xrange(randint(1, len(backup))):
+                                board, backup = restore_board(board, backup)
+                        raise BreakLoop
+
+                    # Input result in the grid or increment "errors" counter
+                    elif len(board[(x, y)]) == 1:
+                        board[(x, y)] = board[(x, y)][0]
+                        no_change = False
+                        raise BreakLoop
+
+    except BreakLoop:
+        continue
+
+    # If Inclusion solver doesn't find any more matches (ie. lists
+    # of length 1)
+    if no_change:
+
+        # Find a random unsolved item on the grid
+        # with the fewest potential solutions
+
+        # 1st, keep only unsolved items (= lists)
+        pending_values = list()
+        for value in board.values():
+            if isinstance(value, list):
+                pending_values.append(value)
+
+        if len(pending_values) == 0:
+            not_solved = False
+            break
+
+        # 2nd, find out the length of the smallest unsolved item
+        min_solutions = len(min(pending_values, key=lambda z: len(z)))
+
+        # Finally, randomly find an item with min_solutions
+        while True:
+            x = randint(1, 9)
+            y = randint(1, 9)
+
+            if (isinstance(board[(x, y)], list)
+                    and len(board[(x, y)]) == min_solutions):
+                # Backup the board in its current state
+                backup.append(dict(board))
+
+                # Choose a random solution from the item list
+                num = board[(x, y)][randint(0, min_solutions - 1)]
+                board[(x, y)] = int(num)
+
+                break
+
+
+while True:
+    answer = raw_input("Print the (c)omplete board or only its (s)olution? > ")
+    
+    if answer == "c":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print 'Complete board:'
+        board_util.printboard(board)
+
+        raw_input("Press enter to display only the solution values. ")
+        break
+    elif answer == "s":
+        break
+    else:
+        continue
+
+os.system('cls' if os.name == 'nt' else 'clear')
 print 'Solution only:'
 for coord in initial_values:
     if coord in board:
         board[coord] = ' '
 
 board_util.printboard(board)
+
+print '-' * 3
+print "Thanks for using Tom's Lazy Sudoku Solver!"
+print
